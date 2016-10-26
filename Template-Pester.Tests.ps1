67eca8b2cc138872d154d26a8fde8cfc7e5e1f3b
Paul Broadwith <paul@pauby.com>
2016-10-26 17:25:37 +0100

    Updated

---
+++ Template-Pester.Tests.ps1
@@ -0,0 +1,24 @@
+Describe "Testing Function - $($Function.Name) - Functional Processing & Logic" {
+    InModuleScope <MODULE> {
+        Context "Testing parameters input" {   
+            It "Passes testing for null and / or missing mandatory parameter" {    
+            }
+
+            It "Passes testing for invalid parameters" {
+            }
+			
+			It "Passes testing for valid parameters" {
+			}
+        }
+
+        Context "Testing function processing and logic" {
+            It "Passes testing of invalid data" {
+
+            }
+
+            It "Passes testing of valid data" {
+
+            }
+        }
+    }
+}
